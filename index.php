e3ad4a13c192ce02fc8056135e5bca17ed1e70d6
KamauAdrian <adriankubai1999@gmail.com>
2020-02-19 09:30:58 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Adrian</title>
+    <style type="text/css">
+        body, html
+        {
+            margin: 0; padding: 0; height: 100%; overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+<!--<iframe width="100%" height="100%" src="dashboard.html" />-->
+<iframe width="100%" height="100%" src="http://d9c24252fce8d4.in04.hostcontrol.com/" />
+</body>
+</html>
+<?php
+?>
