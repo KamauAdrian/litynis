6c6d03550862a2e6290ced87aafdcab2b0be83d3
KamauAdrian <adriankubai1999@gmail.com>
2020-02-25 11:07:26 +0300

    finalize

--- dashboard.php
+++ dashboard.php
@@ -1,15 +1,18 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Adrian</title>
+    <title>LITYNIS</title>
+
+
     <style type="text/css">
         body, html
         {
             margin: 0; padding: 0; height: 100%; overflow: hidden;
         }
     </style>
+
 </head>
 <body>
-<!--<iframe width="100%" height="100%" src="dashboard.html" />-->
+<!--<iframe width="100%" height="100%" src="dashboard.php" />-->
 <iframe width="100%" height="100%" src="index.html" />
 </body>
 </html>
